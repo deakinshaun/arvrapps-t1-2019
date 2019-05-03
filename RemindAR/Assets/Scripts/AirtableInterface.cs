04f9e34b6594f7dbe7b510937d8e1efb23e45cd7
AIDAN SEAN TESSELAAR <astessel@deakin.edu.au>
2019-05-03 13:50:35 +1000

    Added retriever for database

--- RemindAR/Assets/Scripts/AirtableInterface.cs
+++ RemindAR/Assets/Scripts/AirtableInterface.cs
@@ -9,18 +9,81 @@ using AirtableApiClient;
 public class AirtableInterface : MonoBehaviour
 {
 
-    readonly string baseId = "";
+    readonly string baseId = "appxWREHbWVRhUrMR";
     readonly string appKey = "key6kaWf02hedLgNZ";
+    readonly string tableName = "RemindAR";
 
-    // Start is called before the first frame update
-    void Start()
+    string filterFormula = "";
+    IEnumerable<string> filterFields = new List<string>() {"ID", "Title", "Content"};
+    private bool f_connectingToAirtable = false;
+
+    public void GetData()
     {
-        
+        if (!f_connectingToAirtable)
+        {
+            getData();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    async private void getData()
     {
+        f_connectingToAirtable = true;
+        string offset = null;
+        string errorMessage = null;
+        var records = new List<AirtableRecord>();
+
+        using (AirtableBase airtableBase = new AirtableBase(appKey, baseId))
+        {
+        //
+        // Use 'offset' and 'pageSize' to specify the records that you want
+        // to retrieve.
+        // Only use a 'do while' loop if you want to get multiple pages
+        // of records.
+        //
+            Task<AirtableListRecordsResponse> task = airtableBase.ListRecords(
+                tableName, 
+                "", 
+                filterFields, 
+                filterFormula, 
+                10, 
+                10);
+
+            AirtableListRecordsResponse response = await task;
+
+            if (response.Success)
+            {
+                records.AddRange(response.Records.ToList());
+                offset = response.Offset;
+            }
+            else if (response.AirtableApiError is AirtableApiException)
+            {
+                errorMessage = response.AirtableApiError.ErrorMessage;
+            }
+            else
+            {
+                errorMessage = "Unknown error";
+            }
+        }
         
+
+        if (!string.IsNullOrEmpty(errorMessage))
+        {
+            // Error reporting
+        }
+        else
+        {
+            // Do something with the retrieved 'records' and the 'offset'
+            // for the next page of the record list.
+            foreach (AirtableRecord _atr in records)
+            {
+                var _title = _atr.Fields["Title"];
+                var _content = _atr.Fields["Content"];
+
+                Debug.Log(_title + " " + _content);
+            }
+        }
+
+        f_connectingToAirtable = false;
     }
+    
 }
