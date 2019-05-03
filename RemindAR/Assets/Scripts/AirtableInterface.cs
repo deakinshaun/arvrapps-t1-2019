081e6d16fdf92fc8bf40d8bf625c9fc65eb205f6
AIDAN SEAN TESSELAAR <astessel@deakin.edu.au>
2019-05-03 12:15:03 +1000

    Added airtable support

---
+++ RemindAR/Assets/Scripts/AirtableInterface.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEngine;
+using AirtableApiClient;
+
+
+public class AirtableInterface : MonoBehaviour
+{
+
+    readonly string baseId = "";
+    readonly string appKey = "key6kaWf02hedLgNZ";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
