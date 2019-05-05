3603feb4c514225b3330390d8aa3f3ef749793f2
Amshu <amshumaanrakshith@gmail.com>
2019-05-05 21:53:03 +1000

    Made trail renderer work only on objects with layer wall - Added a new building model - Deleted the old model - Added a basic movement script for testing collisions

---
+++ ArchViz Group/ArchViz App/Assets/Scripts/DrawingComponent/CollisionTester.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionTester : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+        GetComponent<Rigidbody>().AddForce(Vector3.forward * 10f * Input.GetAxis("Horizontal"), ForceMode.Acceleration);
+       
+        GetComponent<Rigidbody>().AddForce(Vector3.left * 10f * Input.GetAxis("Vertical"), ForceMode.Acceleration);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.up * 5f, ForceMode.Impulse);
+        }
+    }
+}
--- ArchViz Group/ArchViz App/Assets/Scripts/DrawingComponent/FingerTrail.cs
+++ ArchViz Group/ArchViz App/Assets/Scripts/DrawingComponent/FingerTrail.cs
@@ -4,17 +4,34 @@ using UnityEngine;
 
 public class FingerTrail : MonoBehaviour
 {
+    [SerializeField]
+    Camera camera;
+
     void Update()
     {
         if(((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved) || Input.GetMouseButton(0)))
         {
-            Plane objPlane = new Plane(Camera.main.transform.forward * -1, this.transform.position);
+            //Plane objPlane = new Plane(Camera.main.transform.forward * -1, this.transform.position);
 
-            Ray mRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-            float rayDistance;
-            if (objPlane.Raycast(mRay, out rayDistance))
+            //Ray mRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            //float rayDistance;
+            //if (objPlane.Raycast(mRay, out rayDistance))
+            //{
+            //    this.transform.position = mRay.GetPoint(rayDistance);
+            //}
+
+            RaycastHit hit;
+            //Ray ray = camera.ScreenPointToRay(Input.GetTouch(0).position);
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
             {
-                this.transform.position = mRay.GetPoint(rayDistance);
+                Debug.Log(hit.collider.name + " was hit.");
+                if (hit.collider != null && hit.collider.gameObject.layer == LayerMask.NameToLayer("Wall"))
+                {
+                    Debug.DrawRay(ray.origin, hit.point * -1, Color.yellow);
+                    this.transform.position = hit.point;
+                }
             }
         }
     }
