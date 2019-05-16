210010157ea18ae8576a45bdf7ba5d0049a93aa7
COREY HENDERSON <cwhen@deakin.edu.au>
2019-05-16 20:31:48 +1000

    Modified Menu selections. Added disabling script for the button the user is currently in.

---
+++ ArchViz Group/ArchViz App/Assets/Scripts/Menu Scripts/SetButtonStates.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SetButtonStates : MonoBehaviour
+{
+    List<GameObject> children;
+
+    void Start()
+    {
+        children = new List<GameObject>();
+        foreach (Transform t in transform)
+        {
+            children.Add(t.gameObject);
+        }
+
+        int sceneID = SceneManager.GetActiveScene().buildIndex;
+
+        for (int i = 0; i < children.Count; i++)
+        {
+            string objectName = "Button" + (i + 1);
+            Button button = GameObject.Find(objectName).GetComponent<Button>();
+            button.interactable = true;
+
+            if (i == sceneID)
+            {
+                button.interactable = false;
+            }
+        }
+    }
+}
