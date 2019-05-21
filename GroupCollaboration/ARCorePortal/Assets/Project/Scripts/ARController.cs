cf03fbeb7ba645517bb7c32df601422eeed3aa18
Flyinsorrow <49112875+Flyinsorrow@users.noreply.github.com>
2019-05-22 03:04:26 +1000

    Newest version
    Uploaded the newest version

---
+++ GroupCollaboration/ARCorePortal/Assets/Project/Scripts/ARController.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+using UnityEngine.UI;
+using System.Linq;
+
+//#if UNITY_EDITOR
+//using input = GoogleARCore.InstantPreviewInput;
+//#endif
+
+public class ARController : MonoBehaviour {
+
+    //A list of planes which ARCore detected in the current frame
+    private List<TrackedPlane> m_NewTrackedPlanes = new List<TrackedPlane>();
+
+    public GameObject GridPrefab;
+
+    //The portal scene
+    public GameObject Portal;
+
+    public GameObject PanoramaSphere;
+
+    //the user camera
+    public GameObject ARCamera;
+
+    //Notification text
+    public Text TextNotification;
+    private bool touched = false;
+
+    public Button closePortal;
+
+    Renderer panoRender;
+
+    public Texture2D[] PanoTextures;
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        userNotification(touched);
+
+        //check arcore session status
+        if (Session.Status != SessionStatus.Tracking)
+        {
+            return;
+        }
+
+        //Fill m_NewTrackedPlanes with the planes that ARCore detected in the current frame
+        Session.GetTrackables<TrackedPlane>(m_NewTrackedPlanes, TrackableQueryFilter.New);
+
+        //
+        for (int i = 0; i < m_NewTrackedPlanes.Count; i++)
+        {
+            GameObject grid = Instantiate(GridPrefab, Vector3.zero, Quaternion.identity, transform);
+
+            //Set the position of grid and modify the vertices of the attached mesh
+            grid.GetComponent<GridVisualiser>().Initialize(m_NewTrackedPlanes[i]);
+        }
+
+        //check if the user touched the screen
+        Touch touch;
+        if (Input.touchCount < 1 || (touch = Input.GetTouch(0)).phase != TouchPhase.Began)
+        {
+            touched = true;
+            return;
+        }
+        else
+        {
+            touched = false;
+        }
+
+
+
+        //Now check if the user touched any of the tracked planes
+        TrackableHit hit;
+        if (Frame.Raycast(touch.position.x, touch.position.y, TrackableHitFlags.PlaneWithinPolygon, out hit))
+        {
+            
+            //Now place the portal on the top of the tracked plane user touched
+
+            //enable portal
+            Portal.SetActive(true);
+
+            //Create a new Anchor
+            Anchor anchor = hit.Trackable.CreateAnchor(hit.Pose);
+
+            //Set the portal position to where user hit
+            Portal.transform.position = hit.Pose.position;
+            Portal.transform.rotation = hit.Pose.rotation;
+
+            //Let the portal face the camera
+            Vector3 cameraPosition = ARCamera.transform.position;
+
+            //The portal should only rotate on Y axis
+            cameraPosition.y = hit.Pose.position.y;
+
+            //Rotate the portal to face the camera
+            Portal.transform.LookAt(cameraPosition, Portal.transform.up);
+
+            //Attach the portal to anchor
+            Portal.transform.parent = anchor.transform;
+        }
+
+    }
+
+    public void userNotification(bool t)
+    {
+
+        if (t && Portal.activeSelf)
+        {
+            TextNotification.text = "Portal Spawned!";
+        }
+        else if (!Portal.activeSelf)
+        {
+            TextNotification.text = "Detecting planes... Tap a grid to open portal"; ;
+        }
+
+    }
+
+    public void ClosePortal()
+    {
+        Portal.SetActive(false);
+    }
+
+    int i = 0;
+    public void nextPanorama()
+    {
+        ++i;
+        panoRender = PanoramaSphere.GetComponent<Renderer>();
+        panoRender.sharedMaterial.EnableKeyword("_MainTex");
+        panoRender.sharedMaterial.SetTexture("_MainTex", PanoTextures[Mathf.Abs(i % PanoTextures.Length)]);
+    }
+
+    public void previousPanorama()
+    {
+        --i;
+        panoRender = PanoramaSphere.GetComponent<Renderer>();
+        panoRender.sharedMaterial.EnableKeyword("_MainTex");
+        panoRender.sharedMaterial.SetTexture("_MainTex", PanoTextures[Mathf.Abs(i % PanoTextures.Length)]);
+    }
+
+
+}
+
---
+++ GroupCollaboration/ARCorePortal/Assets/Project/Scripts/PortalManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class PortalManager : MonoBehaviour {
+
+    public GameObject MainCamera;
+
+    public GameObject PortalScene;
+
+    private Material[] PortalStruct;
+
+
+	// Use this for initialization
+	void Start () {
+        PortalStruct = PortalScene.GetComponent<Renderer>().sharedMaterials;
+
+        for (int i = 0; i < PortalStruct.Length; ++i)
+        {
+            PortalStruct[i].SetInt("_StencilComp", (int)CompareFunction.Equal);
+        }
+    }
+	
+	// Update is called once per frame
+	void OnTriggerStay (Collider collider) {
+
+        //Vector3 camPositionInPortalSpace = transform.InverseTransformPoint(MainCamera.transform.position);
+        Vector3 camPositionInPortalSpace1 = this.transform.position - MainCamera.transform.position;
+        float z = camPositionInPortalSpace1.z;
+
+        Debug.Log("camPositionInPortalSpace.z = " + z);
+
+        if (z > -0.6f) //very close or already entered portal
+        {
+            //Disable stencil test
+            for (int i = 0; i < PortalStruct.Length; ++i)
+            {
+                PortalStruct[i].SetInt("_StencilComp", (int)CompareFunction.Always);
+            }
+        }
+        else
+        {
+            //Eable stencil test
+            for (int i = 0; i < PortalStruct.Length; ++i)
+            {
+                PortalStruct[i].SetInt("_StencilComp", (int)CompareFunction.Equal);
+            }
+        }
+	}
+
+    /*
+    //set it to not show the panorama scene when spawned
+    private void OnEnable()
+    {
+        //Disable stencil test
+        for (int i = 0; i < PortalStruct.Length; ++i)
+        {
+            PortalStruct[i].SetInt("_StencilComp", 3);
+        }
+    }
+    */
+
+
+    ////set it to not show the panorama scene when disabled
+    private void OnDisable()
+    {
+        //Disable stencil test
+        for (int i = 0; i < PortalStruct.Length; ++i)
+        {
+            PortalStruct[i].SetInt("_StencilComp", 3);
+        }
+    }
+
+}
